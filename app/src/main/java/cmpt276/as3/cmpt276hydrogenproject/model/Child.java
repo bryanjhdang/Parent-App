579d382aef6896d7914edabb3f7ac5abc234d87f
Bry-D <bryandang10@gmail.com>
2021-10-23 15:20:46 -0700

    Create Child and ChildManager class inside model package

---
+++ app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/model/Child.java
@@ -0,0 +1,17 @@
+package cmpt276.as3.cmpt276hydrogenproject.model;
+
+public class Child {
+    String name;
+
+    public Child(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
