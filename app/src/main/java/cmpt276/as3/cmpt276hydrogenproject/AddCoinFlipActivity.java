405bfcd09a558c830f5063075a55d99cc4a553ec
elc10 <ethan_chou@sfu.ca>
2021-10-28 13:35:16 -0700

    [Ethan] in progress of making spinners for adding coinflips

---
+++ app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/AddCoinFlipActivity.java
@@ -0,0 +1,43 @@
+package cmpt276.as3.cmpt276hydrogenproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cmpt276.as3.cmpt276hydrogenproject.model.Child;
+import cmpt276.as3.cmpt276hydrogenproject.model.ChildManager;
+
+public class AddCoinFlipActivity extends AppCompatActivity {
+    private ChildManager childManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coinflip_activity);
+        choosingChildSpinner();
+    }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, CoinflipActivity.class);
+    }
+
+    public void choosingChildSpinner() {
+        Spinner choosingChildSpinner = findViewById(R.id.choosingChildSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                childManager.getListOfChildrenNames()
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        choosingChildSpinner.setAdapter(adapter);
+
+        //choosingChildSpinner.setSelected(childManager.getCoi);
+        choosingChildSpinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+    }
+}
+
+
