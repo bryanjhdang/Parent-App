2b80cd3877ddd8f4f8a0c1d11ba578a12fe15a3f
Bry-D <bryandang10@gmail.com>
2021-11-04 21:56:16 -0700

    Finished UI for menu activity and add color for other activities

--- app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/MainActivity.java
+++ app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/MainActivity.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        getSupportActionBar().hide();
         sp = getSharedPreferences("Hydrogen", MODE_PRIVATE);
 
         loadChildren();
@@ -41,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         toConfigureBtn();
         toCoinflipBtn();
         toTimeoutBtn();
+
+//        TextView tempText = findViewById(R.id.menuTitle);
     }
 
     void toConfigureBtn() {
