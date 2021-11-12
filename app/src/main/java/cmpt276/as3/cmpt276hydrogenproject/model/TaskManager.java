c068452d9bde80862dc59046de8424ae72e23bf7
kevinchen2002 <kaifengchen2002@gmail.com>
2021-11-12 12:33:31 -0800

    [Kevin] Set up TaskManager model class.

--- app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/model/TaskManager.java
+++ app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/model/TaskManager.java
@@ -1,4 +1,27 @@
 package cmpt276.as3.cmpt276hydrogenproject.model;
 
+import java.util.ArrayList;
+
 public class TaskManager {
+    private ArrayList<Task> TASK_LIST = new ArrayList<>();
+    private static TaskManager instance;
+
+    public static TaskManager getInstance() {
+        if (instance == null) {
+            instance = new TaskManager();
+        }
+        return instance;
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return TASK_LIST;
+    }
+
+    public void setTaskListT(ArrayList<Task> taskList) {
+        this.TASK_LIST = taskList;
+    }
+
+    public void addTask(Task newTask) {
+        TASK_LIST.add(newTask);
+    }
 }
