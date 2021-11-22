0b8bc7a88f0115c9820030dac2b9944140929616
kevinchen2002 <kaifengchen2002@gmail.com>
2021-11-22 13:24:01 -0800

    [Kevin] Implemented child IDs for children; no longer relies on multiple referencing to tell if a child is the same, since references are lost in serialization.
    This should be on a branch but I accidentally did this on master.

--- app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/model/Child.java
+++ app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/model/Child.java
@@ -5,14 +5,28 @@ import android.net.Uri;
 
 import androidx.annotation.NonNull;
 
-public class  Child {
-    String name;
+import java.util.Random;
 
+import cmpt276.as3.cmpt276hydrogenproject.R;
+
+public class Child {
+    String name;
+    int childID;
     String uriProfilePicture;
 
     public Child(String name, String profilePicture) {
         this.name = name;
+        generateID();
         this.uriProfilePicture = profilePicture;
+    }
+
+    private void generateID() {
+        Random random = new Random();
+        this.childID = random.nextInt(1000000);
+    }
+
+    public int getChildID() {
+        return childID;
     }
 
     public String getName() {
