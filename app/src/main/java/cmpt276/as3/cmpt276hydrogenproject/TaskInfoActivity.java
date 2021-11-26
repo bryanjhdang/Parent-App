aa28ef7809d4702261337becd667cd322c214788
Ethan <elc10@sfu.ca>
2021-11-25 18:44:05 -0800

    [Ethan] change task info depending on which task is tapped. (null checks in progress)

--- app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/TaskInfoActivity.java
+++ app/src/main/java/cmpt276/as3/cmpt276hydrogenproject/TaskInfoActivity.java
@@ -4,32 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Objects;
 
+import cmpt276.as3.cmpt276hydrogenproject.model.ChildManager;
 import cmpt276.as3.cmpt276hydrogenproject.model.Task;
 import cmpt276.as3.cmpt276hydrogenproject.model.TaskManager;
 
 public class TaskInfoActivity extends AppCompatActivity {
-
-    private final String TITLE_MSG = "actionBarTitle";
-    private final String TASK_INDEX_MSG = "taskIndex";
-    private final String EDIT_CHILD = "Manage Task Settings";
 
     private String actionBarTitle;
     private Task task;
 
     private TaskManager taskManager = TaskManager.getInstance();
 
+    private final String TITLE_MSG = "actionBarTitle";
+    private final String TASK_INDEX_MSG = "taskIndex";
+    private final String EDIT_CHILD = "Manage Task Settings";
+
+    private ImageView assignedChildPicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_info_activity);
-
         initializeIntentInfo();
+
+        assignedChildPicture = findViewById(R.id.taskChildPicture);
+
         setActionBar();
+        setTaskInformation();
     }
 
     private void initializeIntentInfo() {
@@ -44,6 +54,25 @@ public class TaskInfoActivity extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
                 .getColor(R.color.darker_navy_blue)));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setTaskInformation() {
+        //TODO: create checks for null children/tasks in each of the functions below
+        setChangeTaskNameInput();
+        setNameOfAssignedChild();
+    }
+    private void setChangeTaskNameInput() {
+        EditText taskNameInput = findViewById(R.id.taskHeading);
+        taskNameInput.setText(task.getTaskName());
+        Bitmap assignedChildProfilePic = ChildManager.decodeToBase64(task.getCurrentChild().getStringProfilePicture());
+        assignedChildPicture.setImageBitmap(assignedChildProfilePic);
+    }
+
+    private void setNameOfAssignedChild() {
+        TextView nameOfAssignedChild = findViewById(R.id.taskAssignedName);
+        String nameMessage = "This task is assigned to: ";
+        nameMessage += task.getChildName();
+        nameOfAssignedChild.setText(nameMessage);
     }
 
     public static Intent makeIntent(Context context) {
